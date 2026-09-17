832f8e5b24ded04515755b1727ffabaa39573e6d
Buranyashka <alex0220anisimov@gmail.com>
2026-09-17 21:18:32 +0300

    сделаны 1 и 2 задания

---
+++ practice-01/js/types.js
@@ -0,0 +1,51 @@
+"use strict";
+
+const result_1 = "8"+2;
+console.log("Результат_1:", result_1);
+console.log("Тип результата_1:", typeof result_1);
+console.log();
+
+const result_2 = "8"-2;
+console.log("Результат_2:", result_2);
+console.log("Тип результата_2:", typeof result_2);
+console.log();
+
+const result_3 = Number("8") + 2;
+console.log("Результат_3:", result_3);
+console.log("Тип результата_3:", typeof result_3);
+console.log();
+
+const result_4 = "12" > "3";
+console.log("Результат_4:", result_4);
+console.log("Тип результата_4:", typeof result_4);
+console.log();
+
+const result_5 = 12 === "12";
+console.log("Результат_5:", result_5);
+console.log("Тип результата_5:", typeof result_5);
+console.log();
+
+const result_6 = Number("");
+console.log("Результат_6:", result_6);
+console.log("Тип результата_6:", typeof result_6);
+console.log();
+
+const result_7 = Number("text");
+console.log("Результат_7:", result_7);
+console.log("Тип результата_7:", typeof result_7);
+console.log();
+
+const result_8 = Boolean("false");
+console.log("Результат_8:", result_8);
+console.log("Тип результата_8:", typeof result_8);
+console.log();
+
+const result_9 = typeof null;
+console.log("Результат_9:", result_9);
+console.log("Тип результата_9:", typeof result_9);
+console.log();
+
+const result_10 = typeof NaN;
+console.log("Результат_10:", result_10);
+console.log("Тип результата_10:", typeof result_10);
+console.log();
